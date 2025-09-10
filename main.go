e03bdb977eea72d461186fd19b77e9eef5b92646
robert216434 <robertmartonca@yahoo.com>
2025-09-10 22:09:40 +0300

    levenshtein distance

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+)
+
+// LevenshteinDistance calculates the Levenshtein distance between two strings
+func LevenshteinDistance(a, b string) int {
+	la := len(a)
+	lb := len(b)
+	if la == 0 {
+		return lb
+	}
+	if lb == 0 {
+		return la
+	}
+	// Create a 2D slice
+	dp := make([][]int, la+1)
+	for i := range dp {
+		dp[i] = make([]int, lb+1)
+	}
+	for i := 0; i <= la; i++ {
+		dp[i][0] = i
+	}
+	for j := 0; j <= lb; j++ {
+		dp[0][j] = j
+	}
+	for i := 1; i <= la; i++ {
+		for j := 1; j <= lb; j++ {
+			cost := 0
+			if a[i-1] != b[j-1] {
+				cost = 1
+			}
+			dp[i][j] = min(
+				dp[i-1][j]+1,      // deletion
+				dp[i][j-1]+1,      // insertion
+				dp[i-1][j-1]+cost, // substitution
+			)
+		}
+	}
+	return dp[la][lb]
+}
+
+func min(a, b, c int) int {
+	m := a
+	if b < m {
+		m = b
+	}
+	if c < m {
+		m = c
+	}
+	return m
+}
+
+func main() {
+	s1 := "brah"
+	s2 := "bro"
+	dist := LevenshteinDistance(s1, s2)
+	fmt.Printf("Levenshtein distance between '%s' and '%s' is %d\n", s1, s2, dist)
+}
